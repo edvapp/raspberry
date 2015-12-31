c2e7d64d0a67818e2860d93d31618412907b9ddd
Reinhard Fink <reinhard.fink@tsn.at>
2015-12-31 15:34:02 +0100

    add script to set USb to 1200mA

---
+++ setUSBto1200mA.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file="/boot/config.txt"
+
+function saveOriginal()
+{
+	cp $1 $1.history.$(date +%Y%m%d-%N )
+	if ! [ -f $1.original ];
+	then
+		cp $1 $1.original
+	fi
+}
+
+
+if  !grep 'max_usb_current=1' $file;
+	then
+		saveOriginal $file
+		echo "max_usb_current=1" >> $file
+	fi
