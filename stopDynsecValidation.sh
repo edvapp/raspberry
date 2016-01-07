705106f3e38d2a241817f8b8887836e988aa9a5f
Reinhard Fink <reinhard.fink@tsn.at>
2016-01-07 21:15:56 +0100

    added stop dynsec validation

---
+++ stopDynsecValidation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function saveOriginal()
+{
+	cp $1 $1.history.$(date +%Y%m%d-%N )
+	if ! [ -f $1.original ];
+	then
+		cp $1 $1.original
+	fi
+}
+
+file=/etc/bind/named.conf.options
+
+saveOriginal $file
+
+sed -e "{
+	s/dynsec-validation auto/#dynsec-validation auto/
+}" -i $file
